21fbcb873f853aa32251f91dd7f51cfd5fe56ca4
Sebastian Hörl <hoerl.sebastian@gmail.com>
2022-11-02 17:34:03 +0100

    Add example

---
+++ run_example_lyon.sh
@@ -0,0 +1,17 @@
+set -e
+
+## Build connector
+
+cd /home/ubuntu/irtx-matsim-noise-connector/java
+mvn package
+cd /home/ubuntu/irtx-matsim-noise-connector
+
+## Run scenarios
+for scenario in baseline_2022 ucc_2022 ucc_2030; do
+	java -Xmx12g -cp /home/ubuntu/irtx-matsim-noise-connector/java/target/lead-matsim-noise-connector-1.0.0.jar fr.irtx.lead.matsim_noise_connector.RunNoiseConverter \
+	  --configuration-path /home/ubuntu/irtx-matsim-noise-connector/data/configuration_lyon.json \
+	  --network-path /home/ubuntu/irtx-matsim/output/output_${scenario}/output_network.xml.gz \
+	  --plans-path /home/ubuntu/irtx-matsim/output/output_${scenario}/output_plans.xml.gz \
+	  --receivers-path /home/ubuntu/irtx-matsim-noise-connector/output/receivers_${scenario}.csv \
+	  --volumes-path /home/ubuntu/irtx-matsim-noise-connector/output/volumes_${scenario}.csv
+done
